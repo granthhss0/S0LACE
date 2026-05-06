d2672ffbf363b3e3d07f30c6b1cfc90eb2e1d77d
grant <grantschmidt8@gmail.com>
2026-05-06 10:09:51 -0500

    static

--- public/transport-boot.js
+++ public/transport-boot.js
@@ -6,15 +6,9 @@
     // This comes from baremux/index.js
     const connection = new BareMux.BareMuxConnection("/baremux/worker.js");
 
-    const wispUrl =
-      (location.protocol === "https:" ? "wss" : "ws") +
-      "://" +
-      location.host +
-      "/wisp/";
-
-    if ((await connection.getTransport()) !== "/epoxy/index.mjs") {
-      await connection.setTransport("/epoxy/index.mjs", [{ wisp: wispUrl }]);
-    }
+  if ((await connection.getTransport()) !== "/epoxy/index.mjs") {
+  await connection.setTransport("/epoxy/index.mjs", [{ wisp: "wss://bare-server.fly.dev/wisp/" }]);
+}
 
     console.log("BareMux transport ready in this tab");
   } catch (err) {
